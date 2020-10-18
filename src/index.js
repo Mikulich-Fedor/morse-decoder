eb011dda6d2d85ffee7ca582a5ca9af8ca6a0ee6
fiodar miculich <feotheodor@gmail.com>
2020-10-18 16:45:47 +0300

    init: task morse

--- src/index.js
+++ src/index.js
@@ -35,10 +35,25 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoder;    /*Change code on morse( key "MORSE_TABLE")*/
+    let leter;     /*10 simbol*/
+    let result = '';
+    /*looking for key for "MORSE_TABLE"*/
+    for (let i = 0; i < expr.length; i = i + 10) {
+    leter = expr.substring(i, i + 10);
+    decoder = leter
+                .replace(/10/g, '.')
+                .replace(/11/g, '-')
+                .replace(/\*{10}/, ' ')
+                .replace(/00/g, '');
+    
+    result += MORSE_TABLE[decoder];
+    } 
+    return result;
 }
 
 module.exports = {
